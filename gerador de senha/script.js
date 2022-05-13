61e3216c469e316693a05ac4f533b2703a20f9dc
DrakeCarmesin <102532133+DrakeCarmesin@users.noreply.github.com>
2022-05-13 11:03:41 -0300

    final do gerador de senha

---
+++ gerador de senha/script.js
@@ -0,0 +1,43 @@
+let sliderElement = document.querySelector('#slider')
+let buttonElement = document.querySelector('button')
+let dicas = document.querySelector('#dicas')
+
+let sizePassword = document.querySelector('#valor')
+let Password = document.querySelector('#password')
+
+let containerPassword = document.querySelector('#container-password')
+
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$*/"
+let novaSenha = ""
+
+sizePassword.innerHTML = sliderElement.value
+
+slider.oninput = function (){
+    sizePassword.innerHTML = this.value
+}
+
+function generatePassword (){
+    let pass = ""
+
+    for(let i = 0 , n = charset.length; i<sliderElement.value; ++i){
+
+    pass += charset.charAt(Math.floor(Math.random() *n))
+    }
+
+    containerPassword.classList.remove("hide")
+    Password.innerHTML = pass
+    novaSenha = pass
+
+    dicas.innerHTML = "clique na senha para copiar. 👉"
+}
+
+function copyPassword(){
+    
+    navigator.clipboard.writeText(novaSenha)
+    dicas.innerHTML = "senha copiada"
+}
+
+
+
+
